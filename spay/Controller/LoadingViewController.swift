75fcdb46847c65bb39ddd0978dfc4565f37aa649
NextLogic DevOps <system.admin@philwebservices.com>
2022-10-24 20:11:49 +0800

    adding tab controller

---
+++ spay/Controller/LoadingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoadingViewController.swift
+//  spay
+//
+//  Created by Lanz Ruiz on 10/24/22.
+//
+
+import UIKit
+
+
+class LoadingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+    private func setupViews() {
+        
+    }
+}
