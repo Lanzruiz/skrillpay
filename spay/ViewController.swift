9ad4a808324c29855ee9beeaaa9bbb93bfc0a8df
NextLogic DevOps <system.admin@philwebservices.com>
2022-10-24 19:03:09 +0800

    update the app

---
+++ spay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spay
+//
+//  Created by Lanz Ruiz on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
