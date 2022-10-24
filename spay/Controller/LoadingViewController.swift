a552dd36d411ba6685195008c3070b3426a1db6c
NextLogic DevOps <system.admin@philwebservices.com>
2022-10-24 20:51:13 +0800

    adding label on the tab bar menu

--- spay/Controller/LoadingViewController.swift
+++ spay/Controller/LoadingViewController.swift
@@ -9,13 +9,39 @@ import UIKit
 
 
 class LoadingViewController: UIViewController {
+    
+    private let isUserLoggedIn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showInitialView()
+    }
+    
     private func setupViews() {
         
+    }
+    
+    private func showInitialView() {
+        
+        // if user is logged in, => main tab bar controller
+        // if user is not logged in => show onboarding controller
+        
+        if isUserLoggedIn {
+            
+            let mainTabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainTabBarController")
+            
+            let sceneDelegate = view.window?.windowScene?.delegate as! SceneDelegate
+            let window = sceneDelegate.window
+            window?.rootViewController = mainTabBarController
+            
+        } else {
+            performSegue(withIdentifier: "showOnboarding", sender: nil)
+        }
     }
 }
